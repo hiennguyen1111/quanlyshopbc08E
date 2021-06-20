b0597f2958fa837bab2f56eb7932214f778b25bc
Nguyễn Hiền <hiennguyen@Nguyens-MacBook-Pro.local>
2021-06-20 11:20:16 +0700

    tao_nhanh_devB_code_linh_tinh

--- controllers/devB.js
+++ controllers/devB.js
@@ -3,3 +3,7 @@ console.log('dev B code chuc nang nay nha');
 function devB() {
     console.log('Ahihi do ngoc');
 }
+
+function createBranchDevB() {
+    console.log('Vui qua minh vua moi tao nhanh DevB ne!')
+}
